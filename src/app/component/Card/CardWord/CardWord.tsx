49e883dd7d3be47b4a55ace3662806816f419486
Acrylus <theantonjoseph@gmail.com>
2024-07-18 17:13:57 +0800

    Revert

--- src/app/component/Card/CardWord/CardWord.tsx
+++ src/app/component/Card/CardWord/CardWord.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode, useState } from "react";
 import "./cardword.scss";
-import { InputLine } from "../../Input/Input";
 
 interface CardWordProps {
     className?: string;
@@ -8,20 +7,11 @@ interface CardWordProps {
 }
 
 const CardWord: React.FC<CardWordProps> = ({ className, children }) => {
-    const [searchWord, setSearchWords] = useState();
-
     return (
         <main className={className}>
             <section className="cardword-card">
                 <div className="outer">
-                    <div className="inner">
-                        <InputLine
-                            type="text"
-                            placeholder="Search"
-                            value={searchWord}
-                        />
-                        {children}
-                    </div>
+                    <div className="inner">{children}</div>
                 </div>
             </section>
         </main>
