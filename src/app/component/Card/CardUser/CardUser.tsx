6bb930f487903b5441547bde4871795d1e3cb1a3
Acrylus <theantonjoseph@gmail.com>
2024-10-02 03:04:30 +0800

    Exit Adventure - Banner Dashboard - Student Profile - Added userGetUserDetails Hook

--- src/app/component/Card/CardUser/CardUser.tsx
+++ src/app/component/Card/CardUser/CardUser.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./carduser.scss";
 import useStudentInfo from "@/hook/useStudentInfo";
+import fetchUserDetails from "@/hook/useGetUserDetails";
 
 interface CardUserProps {
     username: number;
@@ -17,32 +18,51 @@ const CardUser: React.FC<CardUserProps> = ({
     className,
     onClick,
 }) => {
+    const characterName = (imagePath: string | null | undefined) => {
+        if (!imagePath) {
+            return "unknown";
+        }
+        const match = imagePath.match(/&\w+_(\w+)-a\d+-i\d+/);
+        return match ? match[1] : "unknown";
+    };
+
     const user = useStudentInfo(username);
+
+    const userDetails = fetchUserDetails(username);
+    const profile = characterName(userDetails.userDetails?.equipped_character);
 
     const totalSeconds = time ? Number(time) : 0;
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = (totalSeconds % 60).toFixed(2).padStart(5, "0");
 
-    const formattedTime = `${minutes}:${seconds}`;
+    const formattedTime = !isNaN(totalSeconds) ? `${minutes}:${seconds}` : time;
 
+    console.log(profile);
     console.log(user.studentInfo?.data?.username);
+    console.log(time);
 
     return (
-        <section className={`carduser-card ${className || ""}`} onClick={onClick}>
+        <section
+            className={`carduser-card ${className || ""}`}
+            onClick={onClick}
+        >
             <section className="carduser-container">
                 <section className="banner-container">
-                    <div className="banner"></div>
+                    <div className="banner">
+                        <img
+                            src={`/assets/images/sprite/profile-${profile}.png`}
+                        />
+                    </div>
 
-                    
-                        <div className="details">
-                            {username && (
-                                <h1>{user.studentInfo?.data?.username}</h1>
-                            )}
-                            {time && (
-                                <h4>Average Time: {formattedTime}</h4>
-                            )}
-                        </div>
-                    
+                    <div className="details">
+                        {username && (
+                            <h1>
+                                {user.studentInfo?.data?.lastname},&nbsp;
+                                {user.studentInfo?.data?.firstname}
+                            </h1>
+                        )}
+                        {time && <h4>Time: {formattedTime}</h4>}
+                    </div>
                 </section>
 
                 {score !== undefined && (
