c4e6c647769b22672a3f2bb4e7f07787d2a0f41d
Acrylus <theantonjoseph@gmail.com>
2024-10-02 00:51:03 +0800

    Dashboard and Fix Equip!

--- src/app/(navigation)/dashboard/page.tsx
+++ src/app/(navigation)/dashboard/page.tsx
@@ -1,52 +1,54 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from "react"
-import CardMode from "@/app/component/Card/CardMode/CardMode"
-import "./dashboard.scss"
-import CardArchive from "@/app/component/Card/CardArchive/CardArchive"
-import useUserInfoStore from "@/store/userInfoStore"
-import useProgressDashboardStore from "@/store/progressDashboardStore"
-import getUserDetails from "@/lib/user-endpoint/getUserDetails"
-import viewCreatedRoom from "@/lib/room-endpoint/viewCreatedRoom"
-import { useRoomStore } from "@/store/roomStore"
-import viewStudentRoom from "@/lib/room-endpoint/viewStudentRoom"
-import Loading from "@/app/loading"
+import { useState, useEffect } from "react";
+import CardMode from "@/app/component/Card/CardMode/CardMode";
+import "./dashboard.scss";
+import CardArchive from "@/app/component/Card/CardArchive/CardArchive";
+import useUserInfoStore from "@/store/userInfoStore";
+import useProgressDashboardStore from "@/store/progressDashboardStore";
+import getUserDetails from "@/lib/user-endpoint/getUserDetails";
+import viewCreatedRoom from "@/lib/room-endpoint/viewCreatedRoom";
+import { useRoomStore } from "@/store/roomStore";
+import viewStudentRoom from "@/lib/room-endpoint/viewStudentRoom";
+import Loading from "@/app/loading";
+import useProgressEquippedStore from "@/store/progressEquippedStore";
 
 export default function Dashboard() {
-    const { rooms, setRoom } = useRoomStore()
-    const { userType } = useUserInfoStore.getState()
+    const { rooms, setRoom } = useRoomStore();
+    const { userType } = useUserInfoStore.getState();
     const userDashboard = useProgressDashboardStore(
         (state) => state.progressDashboard
-    )
-    const [loading, setLoading] = useState(true) // Loading state
+    );
+    const [loading, setLoading] = useState(true); // Loading state
+    const { progressEquipped } = useProgressEquippedStore();
 
     useEffect(() => {
-        getUserDetails()
-    }, [])
+        getUserDetails();
+    }, []);
 
     useEffect(() => {
         const fetchRooms = async () => {
             try {
                 if (userType.toLowerCase() === "teacher") {
-                    const roomData = await viewCreatedRoom()
-                    setRoom(roomData)
+                    const roomData = await viewCreatedRoom();
+                    setRoom(roomData);
                 } else {
-                    const roomData = await viewStudentRoom()
-                    setRoom(roomData)
+                    const roomData = await viewStudentRoom();
+                    setRoom(roomData);
                 }
             } catch (error) {
-                console.error("Failed to fetch rooms:", error)
-                setRoom([])
+                console.error("Failed to fetch rooms:", error);
+                setRoom([]);
             } finally {
-                setLoading(false) // Set loading to false after data is fetched
+                setLoading(false); // Set loading to false after data is fetched
             }
-        }
+        };
 
-        fetchRooms()
-    }, [setRoom, userType])
+        fetchRooms();
+    }, [setRoom, userType]);
 
     if (loading) {
-        return <Loading /> // Render Loading component while fetching data
+        return <Loading />; // Render Loading component while fetching data
     }
 
     return (
@@ -83,9 +85,10 @@ export default function Dashboard() {
                 />
             </section>
             <CardArchive
+                badgeEquipped={progressEquipped.equippedBadge}
                 badgesCount={userDashboard.achievementCount}
                 wordsCount={userDashboard.wordsCollected}
             />
         </main>
-    )
+    );
 }
