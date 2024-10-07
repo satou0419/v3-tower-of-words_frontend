2c1fcfac87c7e7e53dbc6e27034f4aa9ae6d6d4e
Rey Dante Garcia <officialgarciareydante@gmail.com>
2024-10-08 00:45:55 +0800

    Partial Input update

--- src/app/component/Input/Input.tsx
+++ src/app/component/Input/Input.tsx
@@ -1,16 +1,23 @@
-import React, { ReactNode, InputHTMLAttributes, forwardRef } from "react"
+import React, {
+    ReactNode,
+    InputHTMLAttributes,
+    forwardRef,
+    useState,
+} from "react"
 import "./input.scss"
 
 // Define the prop types for the main Input container
 interface InputProps {
     children: ReactNode
     className?: string
+    label?: string // Added label here
     [key: string]: any
 }
 
 // Extend InputHTMLAttributes to allow all input props
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
     className?: string
+    label?: string // Added label here
 }
 
 const Input = ({ children, className = "", ...props }: InputProps) => {
@@ -22,27 +29,78 @@ const Input = ({ children, className = "", ...props }: InputProps) => {
 }
 
 const InputBox = forwardRef<HTMLInputElement, InputFieldProps>(
-    ({ className = "", type = "text", ...props }, ref) => {
+    ({ className = "", type = "text", label, ...props }, ref) => {
+        const [isFocused, setIsFocused] = useState(false) // State to track focus
+
+        const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+            setIsFocused(true) // Set focus state
+            if (props.onFocus) props.onFocus(event) // Call onFocus if provided
+        }
+
+        const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+            if (!props.value) setIsFocused(false) // Clear focus if input is empty
+            if (props.onBlur) props.onBlur(event) // Call onBlur if provided
+        }
+
         return (
-            <input
-                type={type}
-                className={`input-box ${className}`}
-                {...props}
-                ref={ref}
-            />
+            <div className="input-wrapper">
+                <input
+                    type={type}
+                    className={`input-box ${className} ${
+                        isFocused ? "focused" : ""
+                    }`}
+                    {...props}
+                    ref={ref}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    placeholder=""
+                />
+                <label
+                    className={`input-label ${
+                        isFocused || props.value ? "filled" : ""
+                    }`}
+                >
+                    {label}
+                </label>
+            </div>
         )
     }
 )
 
 const InputLine = forwardRef<HTMLInputElement, InputFieldProps>(
-    ({ className = "", type = "text", ...props }, ref) => {
+    ({ className = "", type = "text", label, ...props }, ref) => {
+        const [isFocused, setIsFocused] = useState(false) // State to track focus
+
+        const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+            setIsFocused(true) // Set focus state
+            if (props.onFocus) props.onFocus(event) // Call onFocus if provided
+        }
+
+        const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+            if (!props.value) setIsFocused(false) // Clear focus if input is empty
+            if (props.onBlur) props.onBlur(event) // Call onBlur if provided
+        }
+
         return (
-            <input
-                type={type}
-                className={`input-line ${className}`}
-                {...props}
-                ref={ref}
-            />
+            <div className="input-wrapper">
+                <input
+                    type={type}
+                    className={`input-line ${className} ${
+                        isFocused ? "focused" : ""
+                    }`}
+                    {...props}
+                    ref={ref}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+                <label
+                    className={`input-label ${
+                        isFocused || props.value ? "filled" : ""
+                    }`}
+                >
+                    {label}
+                </label>
+            </div>
         )
     }
 )
